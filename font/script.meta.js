56d1fe94da1157e0bdb973e87264f3e95431a839
nxrhn <128985049+2elnwndrer@users.noreply.github.com>
2025-04-17 17:27:32 +0500

    Create script.meta.js

---
+++ font/script.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         BPlusJS
+// @version      1.5.3.12
+// @description  Оптимизация и упрощение работы.
+// @match        https://school.bilimal.kz/*
+// @compatible   firefox
+// @compatible   chrome
+// @compatible   opera
+// @compatible   safari
+// @compatible   edge
+// @license      MIT
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant GM_setClipboard
+// @namespace https://greasyfork.org/users/1269292
+// ==/UserScript==
